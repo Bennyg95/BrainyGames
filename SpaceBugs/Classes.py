392934b69358422fcf739b2f32a21ca06b2867ea
Bennyg95 <sanchez.benitog95@hotmail.com>
2015-05-04 18:15:09 -0700

    Added some comments to the code. Also changed my characters to minions since there nicer.

--- SpaceBugs/Classes.py
+++ SpaceBugs/Classes.py
@@ -7,9 +7,9 @@ class BaseClass(pygame.sprite.Sprite):	#contains every single sprite in the game
 	
 	allsprites = pygame.sprite.Group() #all  ojects that are created 
 	# __init__(self, x, y,  image_string):
-##############################################################################################
+	##########################################################################################
 	def __init__(self, x, y, image_string):
-##############################################################################################
+	##########################################################################################
 
 		pygame.sprite.Sprite.__init__(self) #initializes an object
 		BaseClass.allsprites.add(self)	#adds the object to the all sprites list
@@ -19,27 +19,34 @@ class BaseClass(pygame.sprite.Sprite):	#contains every single sprite in the game
 		self.rect = self.image.get_rect() #gets the dimentions of the image to the rectangle
 		self.rect.x = x	# width of an object
 		self.rect.y = y	# height of an object
+		self.score = 0
 
+	##########################################################################################
 	def destroy(self, ClassName):	#function that will destroy the object
+	##########################################################################################
 
 		ClassName.List.remove(self)	#removes the object from the list
 		BaseClass.allsprites.remove(self)	#removes the object form the list of sprites
 		del self	#deletes the object
 
 ##############################################################################################
-class Bug(BaseClass):
+class Minion(BaseClass):
 ##############################################################################################
 
 	List = pygame.sprite.Group()	#sets list to the sprite group
-	going_right = True	# bugs will move to the right
+	going_right = True	# minions will move to the right
 
+	##########################################################################################
 	def __init__(self, x, y,  image_string):	#initializes a function	
+	##########################################################################################
 		BaseClass.__init__(self, x, y, image_string)	#Initializes the baseclass
-		Bug.List.add(self)	# adds bug created to the List
+		Minion.List.add(self)	# adds bug created to the List
 		self.velx, self.vely = 0, 8	# sets sets corresponding velocities
 		self.jumping, self.go_down = False, False	# sets jumpint and and go dows to false
 
+	##########################################################################################
 	def motion(self, SCREENWIDTH, SCREENHEIGHT):	#sets motion function
+	##########################################################################################
 		predicted_location = self.rect.x + self.velx	# predicts the next location
 
 		if predicted_location < 0:	# if predicted location is out of the screen
@@ -47,51 +54,56 @@ class Bug(BaseClass):
 		elif predicted_location + self.rect.width > SCREENWIDTH:	#if predicted location is out of the screen
 			self.velx = 0	# stop moving
 
-		self.rect.x += self.velx	# keeps bug moving
+		self.rect.x += self.velx	# keeps minion moving
 
 		self.__jump(SCREENHEIGHT)	#call the jump function
 
+	##########################################################################################
 	def __jump(self, SCREENHEIGHT):	# defines the jump function
+	##########################################################################################
 		max_jump = 75	# max height = 75
 
-		if self.jumping:	# if ub is jumping
+		if self.jumping:	# if minion is jumping
 			if self.rect.y < max_jump:	# if current height is less than the max height
 				self.go_down = True	# sets go down to true
-			if self.go_down:	# if bug is going down
-				self.rect.y += self.vely	# add the height of bug
+			if self.go_down:	# if minion is going down
+				self.rect.y += self.vely	# add the height of minion
 
 				predicted_location = self.rect.y + self.vely
 
-				if predicted_location + self.rect.height> SCREENHEIGHT:
-					self.jumping = False
-					self.go_down = False
+				if predicted_location + self.rect.height> SCREENHEIGHT: # if next position is out of range
+					self.jumping = False 	# stop jumping
+					self.go_down = False 	# stop going down
 			else:
-				self.rect.y -= self.vely
+				self.rect.y -= self.vely	#go down
 
 ##############################################################################################
-class Enemy(BaseClass):
+class EvilMinion(BaseClass):
 ##############################################################################################
 	List = pygame.sprite.Group()
 	def __init__(self, x, y, image_string):
 		BaseClass.__init__(self, x, y, image_string)
-		Enemy.List.add(self) # adds enemies to list
-		self.health = 100
+		EvilMinion.List.add(self) # adds enemies to list
+		self.health = 100		# health of evil minion
 
 		self.velx, self.vely= randint(1, 4), 2 # 1 to 4 pixels per second
-		self.amplitude, self.period = randint(20, 140), randint(4, 5) / 100.0
+		self.amplitude, self.period = randint(20, 140), randint(4, 5) / 100.0		# starting amplitude and position is randomly generate
 
 	@staticmethod
+	##########################################################################################
 	def update_all(SCREENWIDTH, SCREENHEIGHT):
-		for enemy in Enemy.List:
-			if enemy.health <= 0:
-				enemy.velx = 0
-				if enemy.rect.y + enemy.rect.height < SCREENHEIGHT:
-					enemy.rect.y += enemy.vely
-			else:
-				enemy.fly(SCREENWIDTH)
+	##########################################################################################
+		for enemy in EvilMinion.List:	# updates all evil minion been created
+			if enemy.health <= 0:	# if health negative
+				enemy.velx = 0	# stop moving horizontally
+				if enemy.rect.y + enemy.rect.height < SCREENHEIGHT:	#if evil minion is still above ground lower it
+					enemy.rect.y += enemy.vely	# lower's minion's vertical position
+			else:	
+				enemy.fly(SCREENWIDTH)	# else keep if moving
 
-
+	##########################################################################################
 	def fly(self, SCREENWIDTH):
+	##########################################################################################
 
 		if self.rect.x + self.rect.width > SCREENWIDTH or self.rect.x < 0:
 			self.image = pygame.transform.flip(self.image, True, False) # flips horizontally
@@ -102,50 +114,57 @@ class Enemy(BaseClass):
 		self.rect.y = self.amplitude * math.sin(self.period * self.rect.x) + 200 #vertical movement
 
 	@staticmethod 
+	##########################################################################################
 	def movement(SCREENWIDTH):
-		for enemy in Enemy.List:
-			enemy.fly(SCREENWIDTH)
+	##########################################################################################
+		for enemy in EvilMinion.List:	# For all evil minions on the screen
+			enemy.fly(SCREENWIDTH)		# keep updating their movement
 
 ##############################################################################################
-class BugProjectile(pygame.sprite.Sprite):
+class MinionProjectile(pygame.sprite.Sprite):
 ##############################################################################################
-	List = pygame.sprite.Group()
+	List = pygame.sprite.Group()	#varaible for all sprites to be created
 
-	normal_list = []
-	fire = True
-
+	normal_list = []	#array to store projectiles
+	fire = True 	# fire set to true
+	##########################################################################################
 	def __init__(self, x, y, if_fire, image_string):
+	##########################################################################################
 		pygame.sprite.Sprite.__init__(self)
 
-		self.image = pygame.image.load(image_string)
+		self.image = pygame.image.load(image_string)	#initializes self.image for each evil minion
 
 		self.rect = self.image.get_rect() #gets the dimentions of the image to the rectangle
-		self.rect.x = x
-		self.rect.y = y
-		self.if_fire = if_fire
-		self.rect.width
+		self.rect.x = x		#x = x
+		self.rect.y = y		#y = y
+		self.if_fire = if_fire	# fire = fire
+		self.rect.width	#gets width
 
 		try:
-			last_element = BugProjectile.normal_list[-1]
-			difference = abs(self.rect.x - last_element.rect.x)
+			last_element = MinionProjectile.normal_list[-1]	# get the last projectile
+			difference = abs(self.rect.x - last_element.rect.x)	# get the abs difference
 
-			if difference < self.rect.width:
-				return 
-		except Exception:
-			pass
+			if difference < self.rect.width:	# if the difference is less than the width of fire
+				return 	# stop moving it
+		except Exception:	#else 
+			pass		#keep it moving
 
-		BugProjectile.normal_list.append(self)
-		BugProjectile.List.add(self)
-		self.velx = None
+		MinionProjectile.normal_list.append(self)		#adds projectile to normal list
+		MinionProjectile.List.add(self)	#adds fire to list
+		self.velx = None	
 
 	@staticmethod
+	##########################################################################################
 	def movement():
-		for projectile in BugProjectile.List:
+	##########################################################################################
+		for projectile in MinionProjectile.List:
 			projectile.rect.x += projectile.velx
 
+	##########################################################################################
 	def destroy(self):
-		BugProjectile.List.remove(self)
-		BugProjectile.normal_list.remove(self)
+	##########################################################################################
+		MinionProjectile.List.remove(self)
+		MinionProjectile.normal_list.remove(self)
 		del self
 
 
--- SpaceBugs/Main.py
+++ SpaceBugs/Main.py
@@ -1,25 +1,25 @@
-import pygame, sys
-from Classes import *
-from process import process
+import pygame, sys			#imports pygame and the system
+from Classes import *		# imports Classes file
+from process import process	#imports process file
 
-pygame.init()
+pygame.init()			#initializes pygame
 pygame.mixer.init()	# for music
 
-SCREENWIDTH, SCREENHEIGHT = 740, 510
-screen = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
-clock = pygame.time.Clock()
-FPS = 24
-total_frames = 0;
+SCREENWIDTH, SCREENHEIGHT = 740, 510	#sets value for screen width and height
+screen = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))	# Initializes screen with its size
+clock = pygame.time.Clock()		#game will run along with clock
+FPS = 24	#Frames per second
+total_frames = 0;	#iniializes frames
 
-background = pygame.image.load("images/space1.png")
+background = pygame.image.load("images/space1.png")		#creates a background
 
-img = pygame.image.load("images/bug1.png")
-imHeight, imWidth = img.get_rect().size
+img = pygame.image.load("minions/shootingminion.png")	# load picture for the minion that will be shooting
+imHeight, imWidth = img.get_rect().size #gets the size of the image
 
-bug = Bug(0, SCREENHEIGHT - imHeight, "images/bug1.png")
+minion = Minion(0, SCREENHEIGHT - imHeight, "minions/shootingminion.png")	# gets image of minion
 
-pygame.mixer.music.load("music/pirates.mp3")
-pygame.mixer.music.play()
+pygame.mixer.music.load("music/pirates.mp3")		# background music
+pygame.mixer.music.play()		# plays the song
 
 # ----------Main Program Loop --------------------
 while True:
@@ -28,24 +28,24 @@ while True:
 ########################################################################
 # LOGIC
 ########################################################################
-	process(bug, FPS, total_frames)
+	process(minion, FPS, total_frames)				# calls the process method passes minion 
 
-	bug.motion(SCREENWIDTH, SCREENHEIGHT)
-	Enemy.update_all(SCREENWIDTH, SCREENHEIGHT)
-	BugProjectile.movement()
-	total_frames += 1
+	minion.motion(SCREENWIDTH, SCREENHEIGHT)		# sets motion to minion
+	EvilMinion.update_all(SCREENWIDTH, SCREENHEIGHT)		#will update game
+	MinionProjectile.movement()						# moves projectiles
+	total_frames += 1								#addes frames
 	
 ########################################################################
 
 ########################################################################
 # DRAW
 ########################################################################
-	screen.blit(background, (0,0))	
-	BaseClass.allsprites.draw(screen)
-	BugProjectile.List.draw(screen)
+	screen.blit(background, (0,0))					#outputs background
+	BaseClass.allsprites.draw(screen)				#draws all sprites
+	MinionProjectile.List.draw(screen)				#draws list of projectiles
 ########################################################################
 	
-	pygame.display.flip()
+	pygame.display.flip()	
 
-	clock.tick(FPS)
+	clock.tick(FPS)	#time in Frames per second
 
